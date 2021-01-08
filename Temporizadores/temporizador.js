ee1638d5d2481cee391a2d99f5792cb7d2a426f7
andresacn <andres.lemme@gmail.com>
2021-01-08 16:08:28 -0300

    lesson completed

---
+++ Temporizadores/temporizador.js
@@ -0,0 +1,27 @@
+
+// console.log("inicio")
+// setTimeout(()=>{
+//     console.log("esto se ejecuta una sola vez")
+// }, 5000)
+
+// setInterval(() => {
+//     console.log("Ejecutando un setinterval, esto se ejecuta indefinidamente cada cierto intervalo de tiempo")
+// }, 2000);
+
+console.log("-------------------------")
+
+// let temporizador = setTimeout(() => {
+//     console.log(new Date().toLocaleTimeString()) 
+// }, 1000);
+
+// clearTimeout(temporizador)
+
+// setInterval(() => {
+//     console.log(new Date().toLocaleTimeString())
+// }, 1000 );
+
+// let temporizador = setInterval(() => {
+//     console.log(new Date().toLocaleTimeString())
+// }, 1000);
+
+// clearInterval(temporizador)
