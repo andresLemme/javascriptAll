dbc92caaa0cd29e1d0df6efa7f1a576fe6726355
andresacn <andres.lemme@gmail.com>
2021-01-11 13:07:34 -0300

    continuous with event loop and asynchronism

---
+++ asincroniaEventLoop/asincronia.js
@@ -0,0 +1,53 @@
+/*
+Procesamiento Single Thread y Multi thread
+Operaciones de CPU y Operaciones de I/O
+Operaciones de Concurrentes y Paralelas
+Operaciones Bloqueantes y No bloqueantes
+Operaciones Sincronas y   Asincronas
+
+*/
+
+/* Código Síncrono Bloquenate */
+
+(() => {
+  console.log("Codigo Sincrono");
+  console.log("Inicio");
+
+  function dos() {
+    console.log("Dos");
+  }
+
+  function uno() {
+    console.log("Uno");
+
+    dos();
+    console.log("tres");
+  }
+  uno();
+  console.log("fin");
+})();
+
+// console.log("####################")
+  /* Código Asinncrono No Bloquenate */
+
+  (() => {
+      console.log("Codigo Asincrono")
+      console.log("Inicio")
+
+      function dos() {
+          setTimeout(function(){
+              console.log("Dos")
+          }, 1000)
+      }
+
+      function uno() {
+          setTimeout(function(){
+              console.log("Uno")
+          }, 0)
+          dos()
+          console.log("tres")
+      }
+
+      uno()
+      console.log("Fin")
+  })()
