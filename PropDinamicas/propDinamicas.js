89ebfb4bde83d195808a10fada7df9b52582a69a
andresacn <andres.lemme@gmail.com>
2021-01-20 16:56:30 -0300

    propDinamicasObjetos test

--- PropDinamicas/propDinamicas.js
+++ PropDinamicas/propDinamicas.js
@@ -1,17 +1,39 @@
 
 
-let aleatorio = Math.round(Math.random()*100 + 5)
-const objUsuario = {
-    propiedad: "Valor",
-    [`id_${aleatorio}`]: "Valor aleatorio"
+// let aleatorio = Math.round(Math.random()*10 + 5)
+// const objUsuario = {
+//     propiedad: "Valor",
+//     [`id_${aleatorio}`]: "Valor aleatorio",
+    
+    
+// }
+
+// console.log(objUsuario)
+
+// const usuarios = ["Andres", "Federico", "Juan", "Pedro", "Carlos"]
+
+// usuarios.forEach((usuario, index) => {
+//     objUsuario[`id_${index}`] = usuario
+// });
+
+// console.log(usuarios)
+
+console.log("-------------")
+
+
+let aleatorios = Math.round(Math.random() * 10 + 5)
+const objetoUsuario = {
+    prop: "valor",
+    [`id:${aleatorios}`] : "Hola Mundo ",
+
 }
+console.log(objetoUsuario)
 
-console.log(objUsuario)
+const personas = ["Andres", "Jose", "Carlos", "Daniel", "Sebastian"]
 
-const usuarios = ["Andres", "Federico", "Juan", "Pedro", "Carlos"]
-
-usuarios.forEach((usuario, index) => {
-    objUsuario[`id_${index}`] = usuario
+personas.forEach((persona, index) => {
+    objetoUsuario[`id_${index}`] = persona
 });
 
-console.log(usuarios)
+console.log(personas)
+
