9ea2797a4d0a4b7b47e9423cc62d23ea8793493c
andresacn <andres.lemme@gmail.com>
2021-01-25 15:59:42 -0300

    APIs lesson

---
+++ APIs/apis.js
@@ -0,0 +1,11 @@
+
+console.log(window)
+console.log(document)
+
+
+let texto = "Hola Mundo soy Andres aprendiendo javaScript"
+const hablar = (texto) => speechSynthesis.
+
+speak(new SpeechSynthesisUtterance(texto))
+
+hablar(texto)
