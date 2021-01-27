3065b2158a3250f34c312a80da1ebca3624dddd8
andresacn <andres.lemme@gmail.com>
2021-01-27 11:30:41 -0300

    DOM lesson 4

--- DOM/dom.js
+++ DOM/dom.js
@@ -25,64 +25,100 @@ console.log("-------- Elemento del DOM-------")
 //Clase 2 DOM
 //nodos - elementos y selectores
 
-//Ya no se utilizan
-console.log(document.getElementsByTagName("li"))
-console.log(document.getElementsByClassName("card"))
-console.log(document.getElementsByName("nombre"))
+// //Ya no se utilizan
+// console.log(document.getElementsByTagName("li"))
+// console.log(document.getElementsByClassName("card"))
+// console.log(document.getElementsByName("nombre"))
 
 
 
-console.log(document.getElementById("menu"))
-console.log(document.querySelector("#menu"))
-console.log(document.querySelector("a"))
-console.log(document.querySelectorAll("a"))
-console.log(document.querySelectorAll("a").length)
-document.querySelectorAll("a").forEach((el) =>console.log(el))
+// console.log(document.getElementById("menu"))
+// console.log(document.querySelector("#menu"))
+// console.log(document.querySelector("a"))
+// console.log(document.querySelectorAll("a"))
+// console.log(document.querySelectorAll("a").length)
+// document.querySelectorAll("a").forEach((el) =>console.log(el))
 
-console.log(document.querySelector(".card"))
-console.log(document.querySelectorAll(".card")[2])
-document.querySelectorAll(".card").forEach((el) => console.log(el))
+// console.log(document.querySelector(".card"))
+// console.log(document.querySelectorAll(".card")[2])
+// document.querySelectorAll(".card").forEach((el) => console.log(el))
 
-console.log(document.querySelector("#menu li"))
-console.log(document.querySelectorAll("#menu li"))
+// console.log(document.querySelector("#menu li"))
+// console.log(document.querySelectorAll("#menu li"))
 
 
 console.log("---------------------------------")
 
 // Atributos y data-Atrrributes
 
-console.log(document.documentElement.lang)
-console.log(document.documentElement.getAttribute("lang"))
+// console.log(document.documentElement.lang)
+// console.log(document.documentElement.getAttribute("lang"))
 
-console.log(document.querySelector(".link-dom").href)
-console.log(document.querySelector(".link-dom").getAttribute("href"))
+// console.log(document.querySelector(".link-dom").href)
+// console.log(document.querySelector(".link-dom").getAttribute("href"))
 
-document.documentElement.lang= "es"
-console.log(document.documentElement.lang)
+// document.documentElement.lang= "es"
+// console.log(document.documentElement.lang)
 
-document.documentElement.setAttribute("lang", "es-MX")
-console.log(document.documentElement.lang)
+// document.documentElement.setAttribute("lang", "es-MX")
+// console.log(document.documentElement.lang)
 
-const linkDom = document.querySelector(".link-dom") 
+// const linkDom = document.querySelector(".link-dom") 
 
-linkDom.setAttribute("target","_blank")
-linkDom.setAttribute("rel", "noopener")
-linkDom.setAttribute("href", "https://www.telekino.com.ar/")
+// linkDom.setAttribute("target","_blank")
+// linkDom.setAttribute("rel", "noopener")
+// linkDom.setAttribute("href", "https://www.telekino.com.ar/")
 
-console.log(linkDom.hasAttribute("rel"))
-linkDom.removeAttribute("rel")
-console.log(linkDom.hasAttribute("rel"))
+// console.log(linkDom.hasAttribute("rel"))
+// linkDom.removeAttribute("rel")
+// console.log(linkDom.hasAttribute("rel"))
 
 //Data-Attributes
 
-console.log(linkDom.getAttribute("data-description"))
-console.log(linkDom.dataset)
-console.log(linkDom.dataset.id)
-console.log(linkDom.dataset.description)
+// console.log(linkDom.getAttribute("data-description"))
+// console.log(linkDom.dataset)
+// console.log(linkDom.dataset.id)
+// console.log(linkDom.dataset.description)
 
-linkDom.setAttribute("data-description", "Modelo de Objeto del Documento")
-console.log(linkDom.dataset.description)
+// linkDom.setAttribute("data-description", "Modelo de Objeto del Documento")
+// console.log(linkDom.dataset.description)
 
-linkDom.dataset.description = "Quiero Ganar el telekino"
+// linkDom.dataset.description = "Quiero Ganar el telekino"
 
-console.log(linkDom.dataset.description)
+// console.log(linkDom.dataset.description)
+
+// console.log("---------------")
+
+//DOM Estilos y Variables CSS
+
+const linkDom = document.querySelector(".link-dom")
+console.log(linkDom.style)
+console.log(linkDom.getAttribute("style"))
+console.log(linkDom.style.backgroundColor)
+console.log(linkDom.style.color)
+console.log(window.getComputedStyle(linkDom))
+console.log(getComputedStyle(linkDom).getPropertyValue("color"))
+
+linkDom.style.setProperty("text-decoration", "none")
+linkDom.style.setProperty("display", "block")
+linkDom.style.textAlign = "center"
+linkDom.style.marginLeft = "auto"
+linkDom.style.marginRight = "auto"
+linkDom.style.padding = "1rem"
+linkDom.style.borderRadius = ".5rem"
+console.log(getComputedStyle(linkDom))
+
+//Variables CSS Custom Properties CSS
+
+const htmlTest = document.documentElement;
+const bodyTest = document.body;
+let varDarkColor = getComputedStyle(htmlTest).getPropertyValue("--dark-color")
+let varYelowColor = getComputedStyle(htmlTest).getPropertyValue("--yelow-color")
+console.log(varDarkColor, varYelowColor)
+
+bodyTest.style.backgroundColor = varDarkColor;
+bodyTest.style.color = varYelowColor
+
+htmlTest.style.setProperty("--dark-color", "pink")
+varDarkColor = getComputedStyle(htmlTest).getPropertyValue("--dark-color")
+bodyTest.style.setProperty("--dark-color", varDarkColor)
