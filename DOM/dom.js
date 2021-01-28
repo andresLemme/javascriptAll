f4e6734ea563551126dd24c0c31adab26ff68534
andresacn <andres.lemme@gmail.com>
2021-01-28 15:54:34 -0300

    DOM lesson 7

--- DOM/dom.js
+++ DOM/dom.js
@@ -260,3 +260,44 @@ meses2.forEach((el) => {
 document.write("<h3>Meses del año con Fragmento</h3>")
 ul5.appendChild(fragmentt)
 document.body.appendChild(ul5)
+console.log("-----Template------")
+
+//Template
+const cardsTemplate = document.querySelector(".cards")
+const template = document.getElementById("template-card").content
+const fragmentto = document.createDocumentFragment()
+cardContent = [
+    {
+        title: "Tecnología",
+        img: "https://placeimg.com/200/200/tech",
+      },
+      {
+        title: "Animales",
+        img: "https://placeimg.com/200/200/animals",
+      },
+      {
+        title: "Arquitectura",
+        img: "https://placeimg.com/200/200/arch",
+      },
+      {
+        title: "Gente",
+        img: "https://placeimg.com/200/200/people",
+      },
+      {
+        title: "Naturaleza",
+        img: "https://placeimg.com/200/200/nature",
+      },
+]
+
+cardContent.forEach((el) =>{
+    template.querySelector("img").setAttribute("src",el.img)
+    template.querySelector("img").setAttribute("alt", el.title)
+    template.querySelector("figcaption").textContent = el.title
+
+    let clone = document.importNode(template, true)
+
+    fragmentto.appendChild(clone)
+})
+
+cardsTemplate.appendChild(fragmentto)
+
