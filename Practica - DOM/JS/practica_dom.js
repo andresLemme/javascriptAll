499161f83579fe10948c9e3021f5c6aef761c0ae
andresacn <andres.lemme@gmail.com>
2021-03-07 16:53:45 -0300

    Alarma completed

--- Practica - DOM/JS/practica_dom.js
+++ Practica - DOM/JS/practica_dom.js
@@ -1,7 +1,8 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
-import { digitalClock, Alarm } from "./reloj.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("assets/alarma.mp3","#activar-alerma", "#esacivar-alarma")
 });
--- Practica - DOM/JS/reloj.js
+++ Practica - DOM/JS/reloj.js
@@ -1,25 +1,41 @@
-
 export function digitalClock(clock, btnPlay, btnStop) {
-
-    let clockTempo;
+  let clockTempo;
 
   document.addEventListener("click", (e) => {
     if (e.target.matches(btnPlay)) {
-        clockTempo = setInterval(() => {
+      clockTempo = setInterval(() => {
         let clockHour = new Date().toLocaleTimeString();
         document.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;
       }, 1000);
       e.target.disabled = true;
     }
 
-
     if (e.target.matches(btnStop)) {
-        clearInterval(clockTempo);
-        document.querySelector(clock).innerHTML = null;
-        document.querySelector(btnPlay).disabled = false;
-
+      clearInterval(clockTempo);
+      document.querySelector(clock).innerHTML = null;
+      document.querySelector(btnPlay).disabled = false;
     }
   });
 }
 
-export function Alarm(params) {}
+export function alarm(sound, btnPlay, btnStop) {
+  let alarmaTempo;
+  let alarmaSound = document.createElement("audio");
+  alarmaSound.src = sound;
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(btnPlay)) {
+      alarmaTempo = setTimeout(() => {
+        alarmaSound.play();
+      }, 1000);
+      e.target.disabled = true;
+    }
+
+    if (e.target.matches(btnStop)) {
+      clearTimeout(alarmaTempo);
+      alarmaSound.pause();
+      alarmaSound.currentTime = 0;
+      document.querySelector(btnPlay).disabled = false;
+    }
+  });
+}
