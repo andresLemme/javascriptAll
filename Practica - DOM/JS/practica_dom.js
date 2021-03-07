88cfe5b7416ae6440d8726da3417529f2e6512ed
andresacn <andres.lemme@gmail.com>
2021-03-07 17:24:50 -0300

    atajos del teclado completed

--- Practica - DOM/JS/practica_dom.js
+++ Practica - DOM/JS/practica_dom.js
@@ -1,8 +1,15 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
+import {shortcuts} from "./eventoTeclado.js"
 
 document.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-  alarm("assets/alarma.mp3","#activar-alerma", "#esacivar-alarma")
+  alarm("assets/alarma.mp3","#activar-alerma", "#esacivar-alarma");
+  
 });
+
+
+document.addEventListener("keydown", (e) =>{
+  shortcuts(e)
+})
