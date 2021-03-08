4fa00dc9df36f32a3f3532d89d59d2ad1ce054ff
andresacn <andres.lemme@gmail.com>
2021-03-08 18:19:43 -0300

    scroll Top completed

---
+++ Practica - DOM/JS/btn_scroll.js
@@ -0,0 +1,23 @@
+export default function scrollTopButton(btn) {
+  const $scrollBtn = document.querySelector(btn);
+
+  window.addEventListener("scroll", (e) => {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      console.log(window.pageYOffset, document.documentElement.scrollTop)
+      if (scrollTop >600) {
+          $scrollBtn.classList.remove("hidden")
+      } else{
+          $scrollBtn.classList.add("hidden")
+      }
+  });
+
+  document.addEventListener("click", (e) => {
+      if (e.target.matches(btn)) {
+          window.scrollTo({
+              behavior:"smooth",
+              top:0
+          })
+      }
+  });
+}
--- Practica - DOM/JS/practica_dom.js
+++ Practica - DOM/JS/practica_dom.js
@@ -2,12 +2,16 @@ import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall, shortcuts } from "./eventoTeclado.js";
 import countDown from "./cuentaRegresiva.js";
+import scrollTopButton from "./btn_scroll.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alerma", "#esacivar-alarma");
-  countDown("countDown", "Feb 01, 2022 18:30:00", "Feliz cumpleaños Andres!!!")
+  countDown(
+  "countDown", 
+  "Jun 03, 2022 18:30:00", "Feliz cumpleaños Andres!!!");
+  scrollTopButton(".scroll-top-btn")
 });
 
 document.addEventListener("keydown", (e) => {
