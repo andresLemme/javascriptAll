fe3d94302f0f90c10378fa0fc8cb85e94415db2b
andresacn <andres.lemme@gmail.com>
2021-05-19 16:22:50 -0300

    creando componentes

--- SPA/app/App.js
+++ SPA/app/App.js
@@ -1,23 +1,29 @@
 import api from "./helpers/wp_api.js";
 import { ajax } from "./helpers/ajax.js";
+import {Title} from "./components/Title.js"
 
 export function App() {
-  document.getElementById(
-    "root"
-  ).innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla Js</h1>`;
-  ajax({
-    url: api.POST,
-    cbSuccess: (posts) => {
-      console.log(posts);
-    },
-  });
+    const $root = document.getElementById("root")
 
-  ajax({
-      url: api.CATEGORIES,
-      cbSuccess: (categories)=>{
-          console.log(categories)
-      }
-  })
+    $root.appendChild(Title())
 
-  console.log(api);
+
+//   document.getElementById(
+//     "root"
+//   ).innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla Js</h1>`;
+//   ajax({
+//     url: api.POST,
+//     cbSuccess: (posts) => {
+//       console.log(posts);
+//     },
+//   });
+
+//   ajax({
+//       url: api.CATEGORIES,
+//       cbSuccess: (categories)=>{
+//           console.log(categories)
+//       }
+//   })
+
+//   console.log(api);
 }
