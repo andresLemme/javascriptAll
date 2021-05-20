5228d12d61cb87bf0c65c6a21b6d699624997dbc
andresacn <andres.lemme@gmail.com>
2021-05-20 12:18:20 -0300

    agregando estilos a los componentes

--- SPA/app/components/SearchForm.js
+++ SPA/app/components/SearchForm.js
@@ -1,5 +1,11 @@
 
 export function SearchForm(){
     const $searchForm = document.createElement("form")
+    const $input = document.createElement("input")
+    $searchForm.classList.add("form-search")
+    $input.name = "search"
+    $input.type = "search"
+    $input.placeholder ="Buscar"
+    $searchForm.appendChild($input)
     return $searchForm
 }
