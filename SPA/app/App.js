0d74ed3d57c0237f67305004af9ca3568cbc02cb
andresacn <andres.lemme@gmail.com>
2021-05-20 13:13:06 -0300

    creacion de posts

--- SPA/app/App.js
+++ SPA/app/App.js
@@ -3,15 +3,28 @@ import { ajax } from "./helpers/ajax.js";
 import { Title } from "./components/Title.js";
 import { Loader } from "./components/Loader.js";
 import { Header } from "./components/Header.js";
-
+import { Posts } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 
 export function App() {
   const $root = document.getElementById("root");
-  
-  
+
   $root.appendChild(Header());
+  $root.appendChild(Posts());
   $root.appendChild(Loader());
-  
+
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+      let html = ""
+      posts.forEach((post) => html += PostCard(post));
+      document.querySelector(".loader").style.display = "none";
+      document.getElementById("posts").innerHTML = html 
+
+    },
+  });
+
   //   document.getElementById(
   //     "root"
   //   ).innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla Js</h1>`;
---
+++ SPA/app/components/PostCard.js
@@ -0,0 +1,12 @@
+export function PostCard(props){
+    return `
+        <article>
+            <img src="https://placeimg.com/200/200/any" alt="Fotos">
+            <h2>Titulo del post</h2>
+            <p>
+            <time datetime="">fecha</time>
+            <a href="#">Ver publicacion</a>
+            </p>
+        <article>
+    `
+}
