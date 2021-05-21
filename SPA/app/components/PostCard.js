accc3bf996950fb73361e6ed63570ef448f2a6c4
andresacn <andres.lemme@gmail.com>
2021-05-21 13:04:40 -0300

    actualizando datos api

--- SPA/app/components/PostCard.js
+++ SPA/app/components/PostCard.js
@@ -1,11 +1,12 @@
 export function PostCard(props){
+    let{date, title, link} = props
     return `
-        <article>
-            <img src="https://placeimg.com/200/200/any" alt="Fotos">
-            <h2>Titulo del post</h2>
+        <article class="post-card">
+            <img src="https://placeimg.com/200/200/any" alt="${title.rendered}">
+            <h2>${title.rendered}</h2>
             <p>
-            <time datetime="">fecha</time>
-            <a href="#">Ver publicacion</a>
+            <time datetime="${date}">${date}</time>
+            <a href="${link}" target="_blank">Ver publicacion</a>
             </p>
         <article>
     `
