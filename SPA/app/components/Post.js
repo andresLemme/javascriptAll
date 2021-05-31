7e5a7edcc332e223166593c76fe863239dd06706
andresacn <andres.lemme@gmail.com>
2021-05-31 16:33:24 -0300

    setup post

---
+++ SPA/app/components/Post.js
@@ -0,0 +1,12 @@
+export function Post(props) {
+    return `
+    <section class="post-page">
+    <aside>
+    <h2>Titulo del post</h2>
+    <time datatime="2020-11-12">223333</time>
+    </aside>
+    <hr>
+    <article>Contenido del Post</article>
+    </section>
+    `
+}
--- SPA/app/components/Router.js
+++ SPA/app/components/Router.js
@@ -1,32 +1,40 @@
 import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js";
+import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
 
 export async function Router() {
   let { hash } = location;
 
-  console.log(hash);
+  // console.log(hash);
 
   if (!hash || hash === "#/") {
     await ajax({
         url: api.POSTS,
         cbSuccess: (posts) => {
-          //   console.log(posts);
+            // console.log(posts);
           let html = " ";
           posts.forEach((post) => (html += PostCard(post)));
           // document.querySelector(".loader").style.display = "none";
           document.getElementById("posts").innerHTML = html;
         },
       });
+      // console.log(api.POST)
   } else if (hash.includes("#/search")) {
     document.getElementById("posts").innerHTML =
       "<h2>Seccion del Buscador</h2>"
   } else if (hash === "#/contacto") {
     document.getElementById("posts").innerHTML =
       "<h2>Seccion de Contacto</h2>"
-  } else {
-    document.getElementById("posts").innerHTML =
-      "<h2>Aqui va contenido del post seleccionado</h2>"
+  } else {  
+      await ajax({
+        url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
+        cbSuccess: (post) => {
+           console.log(post);
+           document.querySelector("post-page").innerHTML = Post(post)
+          
+        },
+      });
   }
   document.querySelector(".loader").style.display = "none";
 
